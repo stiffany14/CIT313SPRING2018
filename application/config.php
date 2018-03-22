26273f37375f6884d66285b2bd286f4ee4010422
stiffany14 <35461619+stiffany14@users.noreply.github.com>
2018-03-22 12:03:31 -0400

    Delete config.php

--- application/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
-define('BASE_URL', '/CIT313/ex5/');
-
-//database info
-define('DB_HOST', 'localhost');
-define('DB_USER', 'stiffany');
-define('DB_PASS', 'stiffany');
-define('DB_NAME', 'stiffany_db');
